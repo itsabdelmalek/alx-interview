5fdef6b3d8a3ff41fcccf3e55ac3aa64132dd504
abdelmalek <abdelmalek@gmail.com>
2024-03-12 14:17:35 +0000

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -49,25 +49,17 @@ def isWinner(x, nums):
     determine who the winner of each game is.
     """
 
-    players = {'Maria': 0, 'Ben': 0}
-    cluster = set()
-    for elem in range(x):
-        nums.sort()
-        num = nums[elem]
-        for i in range(1, num + 1):
-            cluster.add(i)
-            if i == num + 1:
-                break
-        temp = get_Primes(cluster)
+    ben_wins = maria_wins = 0
 
-        if temp % 2 == 0:
-            players['Ben'] += 1
-        elif temp % 2 != 0:
-            players['Maria'] += 1
-
-    if players['Maria'] > players['Ben']:
-        return 'Maria'
-    elif players['Maria'] < players['Ben']:
-        return 'Ben'
-    else:
+    if x <= 0:
         return None
+
+    for i in range(x):
+        if get_Primes(nums[i]) % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if ben_wins == maria_wins:
+        return None
+    return 'Ben' if ben_wins > maria_wins else 'Maria'
